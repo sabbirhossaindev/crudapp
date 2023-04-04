66592a611622b49e5ddfe2008c2b2d559a0f620e
sabbir hossain <mdsabbir477470@gmail.com>
2023-04-04 19:48:13 +0600

    all disign done

--- function.php
+++ function.php
@@ -16,5 +16,18 @@
                 die("database conation error");
             }
         }
+        public function add_data($data){
+            $std_name = $data['std_name'];
+            $std_roll = $data['std_roll'];
+            $std_img = $_FILES['std_img']['name'];
+            $tmp_name = $_FILES['std_img']['tmp_name'];
+
+            $query = "INSERT INTO students(std_name,std_roll,std_img) VALUE ('$std_name',$std_roll, '$std_img')";
+
+            if(mysqli_query($this->conn, $query)){
+                move_uploaded_file($tmp_name, 'upload/'.$std_img);
+                return "Information Added Successfully";
+            }
+        }
     }
 ?>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 ?>
 
 <!doctype html>
-<html lang="en">
+<html lang="en"> 
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
